db23a4f8bb70b4fe59cd1f65d57628009ffb091e
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-08 16:06:52 +0700

    Removed unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Link } from "react-router-dom";
