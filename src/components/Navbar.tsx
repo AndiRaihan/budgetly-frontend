c90a3146b9b63aa84b20d8e9ae35f4e06624e98e
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-12 20:31:27 +0700

    Added login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import ThemeSwitch from "./ThemeSwitch";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { toggleDarkMode } from "../redux/darkModeSlice";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { darkMode } = useSelector((state: RootState) => state);
@@ -14,11 +15,19 @@ export default function Navbar() {
   return (
     <nav className=" bg-background-light-200 flex justify-between items-center p-4 h-14">
       <h1>Menu</h1>
-      <div>
-        <ThemeSwitch checked={darkMode.isDarkMode} onClick={handleSwitch} />
-        <button className="bg-primary-200 w-36 h-7 rounded-md hover:bg-primary-300 transition-all duration-300 focus:ring-4 focus:bg-primary-300 shadow-lg transform active:scale-75 mx-5">
+      <div className="flex flex-row items-center justify-center">
+        <div className="p-2">
+          <ThemeSwitch checked={darkMode.isDarkMode} onClick={handleSwitch} />
+        </div>
+          <a href="/login" className=" py-2 px-5 hover:bg-background-light-300 rounded-md">
+            <h1>Login</h1>
+          </a>
+        <a
+          href="/register"
+          className=" flex items-center justify-center bg-primary-200 w-36 h-7 rounded-md hover:bg-primary-300 transition-all duration-300 focus:ring-4 focus:bg-primary-300 shadow-lg transform active:scale-75 mx-5"
+        >
           Register
-        </button>
+        </a>
       </div>
     </nav>
   );
