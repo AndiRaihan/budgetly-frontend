73077cf2af152d573f2d0c5680145722532acc22
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-12 20:47:18 +0700

    Refactored nav item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "./ThemeSwitch";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { toggleDarkMode } from "../redux/darkModeSlice";
-import { Link } from "react-router-dom";
+import Logo from "./Logo";
 
 export default function Navbar() {
   const { darkMode } = useSelector((state: RootState) => state);
@@ -14,21 +14,20 @@ export default function Navbar() {
 
   return (
     <nav className=" bg-background-light-200 flex justify-between items-center p-4 h-14">
-      <h1>Menu</h1>
-      <div className="flex flex-row items-center justify-center">
-        <div className="p-2">
+      <Logo />
+      <ul className="flex flex-row items-center justify-center">
+        <li className="p-2">
           <ThemeSwitch checked={darkMode.isDarkMode} onClick={handleSwitch} />
-        </div>
-          <a href="/login" className=" py-2 px-5 hover:bg-background-light-300 rounded-md">
-            <h1>Login</h1>
+        </li>
+        <li className=" h-8 w-20 hover:bg-background-light-300 rounded-md flex items-center justify-center">
+          <a className=" flex-1 flex-grow" href="/login">
+            <h1 className=" text-center">Login</h1>
           </a>
-        <a
-          href="/register"
-          className=" flex items-center justify-center bg-primary-200 w-36 h-7 rounded-md hover:bg-primary-300 transition-all duration-300 focus:ring-4 focus:bg-primary-300 shadow-lg transform active:scale-75 mx-5"
-        >
-          Register
-        </a>
-      </div>
+        </li>
+        <li className=" flex items-center justify-center bg-primary-200 w-36 h-7 rounded-md hover:bg-primary-300 transition-all duration-300 focus:ring-4 focus:bg-primary-300 shadow-lg transform active:scale-75 mx-5">
+          <a href="/register">Register</a>
+        </li>
+      </ul>
     </nav>
   );
 }
