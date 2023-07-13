517f2e1abf730a1a7ed4af958862975905ef23b0
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-13 13:31:19 +0700

    logo routing behaviour

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import MoneyBagSVG from '../assets/money-bag.svg';
 
 export default function Logo() {
   return (
-    <a href='/' className='flex'>
+    <Link to='/' className='flex'>
         <img src={MoneyBagSVG} alt="Money Bag" className="w-8 h-8" />
         <h1 className="text-2xl font-bold ml-3">Budgetly</h1>
-    </a>
+    </Link>
   )
 }
