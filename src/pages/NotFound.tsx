b03e8c79418cc9f5b5e7140a73d95700873d1a92
Dave Matthew Peter Lumban Tobing <davetobing4@gmail.com>
2023-07-21 19:36:37 +0700

    page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../redux/store";
+import debt from "../assets/20323-removebg-preview.png"
 
 export default function NotFound() {
   const { darkMode } = useSelector((state: RootState) => state);
@@ -12,7 +13,11 @@ export default function NotFound() {
         darkMode.isDarkMode ? "dark" : ""
       )}
     >
-      <h1 className="my-10 text-center">Page Not Found</h1>
+      <div className="flex justify-center items-center">
+        <img src={debt} alt="" className="w-64" />
+      </div>
+      <h1 className="my-10 text-center">Looks like you've stumbled upon a missing page.
+        We're sorry, but the page you are looking <br /> for cannot be found. It may have been moved, renamed, or deleted  </h1>
       <Button
         className="w-80 self-center"
         variant="outlined"
