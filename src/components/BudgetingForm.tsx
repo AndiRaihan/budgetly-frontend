214089133dd50376b1ed9405ec6ba328ca5242b5
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-24 14:34:59 +0700

    Finished form

--- src/components/BudgetingForm.tsx
+++ src/components/BudgetingForm.tsx
@@ -1,154 +1,171 @@
-import { Switch } from '@mui/material';
-import React from 'react'
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Switch } from "@mui/material";
+import React from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import IconWarning from "../assets/icon _warning_.svg";
+import Period from "../utils/Period";
 
-export default function BudgetingForm({ showForm, setShowForm }: BudgetingFromProps) {
-    type TrackingInput = {
-        trackingName: string;
-        amount: number | null;
-        trackDate: Date;
-        trackType: boolean;
-        category: string;
-      };
-    
-      const {
-        register,
-        handleSubmit,
-        control,
-        reset,
-        formState: { errors, isDirty, isValid },
-      } = useForm<TrackingInput>();
-    
-      const handleReset = () => {
-        setShowForm(false);
-        reset({
-          trackingName: "",
-          amount: null,
-          trackDate: new Date(),
-          trackType: false,
-          category: "Placeholder",
-        });
-      };
-    
-      const onSubmit: SubmitHandler<TrackingInput> = (data) => console.log(data);
-    
-      return (
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className={` ${
-            showForm
-              ? "max-h-screen p-5 m-5 border-2 shadow-md"
-              : "max-h-0 p-0 m-0 shadow-none border-none"
-          } transition-all ease-in-out duration-300 flex flex-col justify-center bg-transparent w-11/12 overflow-hidden`}
-          noValidate
-        >
-          <div className="flex">
+export default function BudgetingForm({
+  showForm,
+  setShowForm,
+}: BudgetingFromProps) {
+  type TrackingInput = {
+    title: string;
+    amount: number | null;
+    period: Period;
+    budgetDate: Date;
+    trackType: boolean;
+    category: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isDirty, isValid },
+    watch,
+  } = useForm<TrackingInput>();
+
+  const handleReset = () => {
+    setShowForm(false);
+    reset({
+      title: "",
+      amount: null,
+      budgetDate: new Date(),
+      trackType: false,
+      category: "Placeholder",
+    });
+  };
+
+  const onSubmit: SubmitHandler<TrackingInput> = (data) => console.log(data);
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={` ${
+        showForm
+          ? "max-h-screen p-5 m-5 border-2 shadow-md"
+          : "max-h-0 p-0 m-0 shadow-none border-none"
+      } transition-all ease-in-out duration-300 flex flex-col justify-center bg-transparent w-11/12 overflow-hidden`}
+      noValidate
+    >
+      <input
+        id="title"
+        type="text"
+        className="bg-transparent placeholder-black focus:placeholder-slate-600"
+        placeholder="Title"
+        {...register("title", {
+          required: "Tracking name is required",
+        })}
+      />
+      <input
+        id="amount"
+        type="number"
+        className="bg-transparent placeholder-black focus:placeholder-slate-600 hover:placeholder-slate-600 text-4xl"
+        placeholder="Target Amount"
+        {...register("amount", {
+          required: "Amount is required",
+          valueAsNumber: true,
+        })}
+      />
+
+      <div className="flex justify-between items-center">
+        <div className="flex">
+          <select
+            className="bg-transparent focus:ring-primary-100 focus:border-primary-100 focus:border px-2 py-1 rounded-md"
+            {...register("period", {
+              required: "Period is required",
+            })}
+          >
+            <option value={Period.Daily} selected>
+              {Period.Daily}
+            </option>
+            <option value={Period.Weekly}>{Period.Weekly}</option>
+            <option value={Period.Monthly}>{Period.Monthly}</option>
+            <option value={Period.Custom}>{Period.Custom}</option>
+          </select>
+          {watch("period") === Period.Custom && (
             <input
-              id="trackingName"
-              type="text"
-              className="bg-transparent placeholder-black focus:placeholder-slate-600"
-              placeholder="Expense/Income Name"
-              {...register("trackingName", {
-                required: "Tracking name is required",
+              id="track-date"
+              type="date"
+              className="bg-transparent"
+              {...register("budgetDate", {
+                required: watch('period') === Period.Custom ? "Date is required" : false,
+                valueAsDate: true,
               })}
             />
-          </div>
-          <input
-            id="amount"
-            type="number"
-            className="bg-transparent placeholder-black focus:placeholder-slate-600 hover:placeholder-slate-600 text-4xl"
-            placeholder="Amount"
-            {...register("amount", {
-              required: "Amount is required",
-              valueAsNumber: true,
+          )}
+          <select
+            className="bg-transparent focus:ring-primary-100 focus:border-primary-100 focus:border px-2 py-1 rounded-md"
+            {...register("category", {
+              required: "Category is required",
+              validate: (value) =>
+                value !== "Placeholder" || "Please select category",
             })}
-          />
-    
-          <div className="flex justify-between items-center">
-            <div className="flex">
-              <input
-                id="track-date"
-                type="date"
-                className="bg-transparent"
-                {...register("trackDate", {
-                  required: "Date is required",
-                  valueAsDate: true,
-                })}
-              />
-              <div className="border border-background-light-400 rounded-md mx-2">
-                <span className="text-black ml-3 mr-0">Income</span>
-                <Controller
-                  name="trackType"
-                  control={control}
-                  defaultValue={false}
-                  render={({ field }) => <Switch {...field} />}
-                />
-                <span className="text-black ml-0 mr-3">Expense</span>
-              </div>
-              <select
-                className="bg-transparent focus:ring-primary-100 focus:border-primary-100 focus:border px-2 py-1 rounded-md"
-                {...register("category", {
-                  required: "Category is required",
-                  validate: (value) =>
-                    value !== "Placeholder" || "Please select category",
-                })}
-              >
-                <option value="Placeholder" disabled selected hidden>
-                  Category
-                </option>
-                <option value="contoh">Contoh</option>
-              </select>
-            </div>
-            <div>
-              <button
-                type="button"
-                className=" bg-gray-400 rounded-md py-1 px-3 hover:brightness-90"
-                onClick={handleReset}
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className={`mx-5 bg-[#FFDA45] rounded-md py-1 px-3 ${
-                  isValid && isDirty ? "hover:brightness-90" : ""
-                } disabled:opacity-60`}
-                disabled={!isDirty || !isValid}
-              >
-                Submit
-              </button>
-            </div>
+          >
+            <option value="Placeholder" disabled selected hidden>
+              Category
+            </option>
+            <option value="contoh">Contoh</option>
+          </select>
+          <div className="mx-2">
+            <Controller
+              name="trackType"
+              control={control}
+              defaultValue={false}
+              render={({ field }) => <Switch {...field} />}
+            />
+            <span className="text-black ml-0 mr-3">Recurring</span>
           </div>
-          {errors.trackingName && (
-            <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
-              <img src={IconWarning} className="w-3 mx-2" />
-              {errors.trackingName.message}
-            </p>
-          )}
-          {errors.amount && (
-            <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
-              <img src={IconWarning} className="w-3 mx-2" />
-              {errors.amount.message}
-            </p>
-          )}
-          {errors.trackDate && (
-            <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
-              <img src={IconWarning} className="w-3 mx-2" />
-              {errors.trackDate.message}
-            </p>
-          )}
-          {errors.category && (
-            <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
-              <img src={IconWarning} className="w-3 mx-2" />
-              {errors.category.message}
-            </p>
-          )}
-        </form>
-      );
-    }
-    
-    type BudgetingFromProps = {
-      showForm: boolean;
-      setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
-    };
-    
+        </div>
+        <div>
+          <button
+            type="button"
+            className=" bg-gray-400 rounded-md py-1 px-3 hover:brightness-90"
+            onClick={handleReset}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className={`mx-5 bg-[#FFDA45] rounded-md py-1 px-3 ${
+              isValid && isDirty ? "hover:brightness-90" : ""
+            } disabled:opacity-60`}
+            disabled={!isDirty || !isValid}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      {errors.title && (
+        <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
+          <img src={IconWarning} className="w-3 mx-2" />
+          {errors.title.message}
+        </p>
+      )}
+      {errors.amount && (
+        <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
+          <img src={IconWarning} className="w-3 mx-2" />
+          {errors.amount.message}
+        </p>
+      )}
+      {errors.budgetDate && (
+        <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
+          <img src={IconWarning} className="w-3 mx-2" />
+          {errors.budgetDate.message}
+        </p>
+      )}
+      {errors.category && (
+        <p className="text-red-600 dark:text-red-500 text-sm flex justify-center items-center self-start">
+          <img src={IconWarning} className="w-3 mx-2" />
+          {errors.category.message}
+        </p>
+      )}
+    </form>
+  );
+}
+
+type BudgetingFromProps = {
+  showForm: boolean;
+  setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
+};
--- src/pages/Budgeting.tsx
+++ src/pages/Budgeting.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import CurrentPage from "../utils/CurrentPage";
+import BudgetingForm from "../components/BudgetingForm";
 
 export default function Budgeting({ translate, changeCurrentPage }: PageProps) {
   changeCurrentPage(CurrentPage.Budgeting);
@@ -24,13 +25,25 @@ export default function Budgeting({ translate, changeCurrentPage }: PageProps) {
           : " translate-x-0 w-screen"
       } transition-all ease-in-out duration-200 overflow-hidden`}
     >
-        <div className="flex justify-between items-end p-1 ml-5 mb-3 w-11/12">
+      <div className="flex justify-between items-end p-1 ml-5 mb-3 w-11/12">
         <h1 className="text-3xl">
           Today <span className="text-sm">{formattedDate}</span>
         </h1>
         {/* TODO: Ubah jadi dropdown Switch  */}
         <h1>Switch</h1>
       </div>
+      <BudgetingForm showForm={showForm} setShowForm={setShowForm} />
+      <button
+        onClick={() => setShowForm((prevState) => !prevState)}
+        className={`${
+          !showForm ? "max-h-max p-1 ml-5" : "max-h-0"
+        } transition-all ease-in-out duration-300  self-start text-start hover:bg-background-light-200 rounded-md w-11/12 overflow-hidden`}
+      >
+        +Add Expense/income
+      </button>
+      {showForm && (
+        <hr className="border rounded-md w-11/12 ml-5 border-black" />
+      )}
     </div>
   );
 }
