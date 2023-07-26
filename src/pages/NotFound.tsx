0d3702ab07ef9b67f9e7a2c326093bb5a92f509b
Dave Matthew Peter Lumban Tobing <davetobing4@gmail.com>
2023-07-26 18:47:33 +0700

    change notfoundpage layout and onClick

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,22 +6,27 @@ import debt from "../assets/20323-removebg-preview.png"
 
 export default function NotFound() {
   const { darkMode } = useSelector((state: RootState) => state);
+
   const navigate = useNavigate();
+
+  const handleLandingPage  = () => {
+    navigate('/');
+  }
   return (
     <div
-      className={"bg-background-light-100 h-screen flex flex-col justify-center items-center".concat(
+      className={"bg-background-light-200 h-screen flex flex-col justify-center items-center".concat(
         darkMode.isDarkMode ? "dark" : ""
       )}
     >
       <div className="flex justify-center items-center">
         <img src={debt} alt="" className="w-64" />
       </div>
-      <h1 className="my-10 text-center">Looks like you've stumbled upon a missing page.
+      <h1 className="my-10 text-center text-dark-green text-xl">Looks like you've stumbled upon a missing page.
         We're sorry, but the page you are looking <br /> for cannot be found. It may have been moved, renamed, or deleted  </h1>
       <Button
         className="w-80 self-center"
         variant="outlined"
-        onClick={() => navigate(-1)}
+        onClick={handleLandingPage}
       >
         Go to Home Page
       </Button>
