b260c917ffc224922ed80a80c88077e4120561d7
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-31 12:02:47 +0700

    Added dummy data

---
+++ src/utils/BudgetingData.ts
@@ -0,0 +1,54 @@
+import { BudgetingBarProps } from "../components/budgeting/BudgetingBar";
+import Period from "./Period";
+
+const data : BudgetingBarProps[] = [
+    {
+        budgetingData: {
+            amount: 2000000,
+            budgetDate: new Date(),
+            category: "contoh",
+            period: Period.Monthly,
+            title: "Pangan",
+            trackType: true,
+        },
+        current: 1000000,
+        showPercent: false,
+    },
+
+    {
+        budgetingData: {
+            amount: 2500000,
+            budgetDate: new Date(),
+            category: "contoh",
+            period: Period.Weekly,
+            title: "Papan",
+            trackType: false,
+        },
+        current: 1500000,
+        showPercent: false,
+    },
+    {
+        budgetingData: {
+            amount: 150000,
+            budgetDate: new Date(),
+            category: "contoh",
+            period: Period.Monthly,
+            title: "Sandang",
+            trackType: false,
+        },
+        current: 50000,
+        showPercent: false,
+    },
+    {
+        budgetingData: {
+            amount: 1_000_000,
+            budgetDate: new Date(),
+            category: "contoh",
+            period: Period.Monthly,
+            title: "Rekreasi",
+            trackType: true,
+        },
+        current: 100_000,
+        showPercent: false,
+    }
+]
