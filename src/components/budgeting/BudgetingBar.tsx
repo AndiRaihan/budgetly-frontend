9a9ef67c9a72d824a8ef6131fc4c47f626b7830d
AndiRaihan <dzaky.raihan@gmail.com>
2023-07-31 12:00:30 +0700

    Refactored props

--- src/components/budgeting/BudgetingBar.tsx
+++ src/components/budgeting/BudgetingBar.tsx
@@ -1,17 +1,18 @@
-type BudgetingBarProps = {
+import EditBudgetingForm, { BudgetingInput } from "./EditBudgetingForm";
+
+export type BudgetingBarProps = {
   showPercent: boolean;
-  title: string;
-  limit: number;
+  budgetingData: BudgetingInput;
   current: number;
 };
 
 export default function BudgetingBar({
   showPercent,
-  title,
-  limit,
+  budgetingData,
   current,
 }: BudgetingBarProps) {
-  const barLength = (current / limit) * 100;
+  const barLength =
+    budgetingData.amount !== null ? (current / budgetingData.amount) * 100 : 0;
   const locale = window.navigator.language;
   const formatter = new Intl.NumberFormat(locale, {
     style: "currency",
@@ -25,11 +26,11 @@ export default function BudgetingBar({
         }}
         className={`absolute bg-primary-200 w-[${barLength}%] h-full z-20 rounded-md top-0 left-0`}
       ></div>
-      <div className="text-white z-30">{title}</div>
+      <div className="text-white z-30">{budgetingData.amount}</div>
       <div className="text-black z-30">
         {showPercent
-          ? `${Math.round((current / limit) * 100)}%`
-          : `${formatter.format(current)}/${formatter.format(limit)}`}
+          ? `${budgetingData.amount !== null ? Math.round((current / budgetingData.amount) * 100) : -1}%`
+          : `${formatter.format(current)}/${budgetingData.amount !== null ? formatter.format(budgetingData.amount) : -1}`}
       </div>
     </div>
   );
