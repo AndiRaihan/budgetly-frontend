474c49589919636a8183bd2f273c9f2b395f9c74
AndiRaihan <dzaky.raihan@gmail.com>
2023-08-07 18:24:12 +0700

    Fixed piechart

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,38 +1,42 @@
 // @ts-ignore
 import CanvasJSReact from "@canvasjs/react-charts";
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import { useEffect, useState } from "react";
 
 export default function PieChart({ items }: PageProps) {
-  // @ts-ignore
-  let chart;
-  const options = {
-      backgroundColor: "transparent",
-      data: [
-        {
-          type: "pie",
-          click: function (e: { dataSeries: { dataPoints: string | any[]; }; dataPoint: { exploded: boolean; }; }) {
-            for (var i = 0; i < e.dataSeries.dataPoints.length; i++) {
-              e.dataSeries.dataPoints[i].exploded = false;
-            }
-            e.dataPoint.exploded = true;
-          },
-          dataPoints: items,
-        },
-      ],
-    },
-    //Styling Chart Container
-    containerProps = {
-      width: "100%",
-      height: "300px",
-    };
-
-  return (
-    <CanvasJSChart
-      options={options}
-      onRef={(ref: any) => (chart = ref)} //Reference to the chart-instance
-      containerProps={containerProps}
-    />
+  const [pieChart, setPieChart] = useState(
+    <h1 style={{ textAlign: "center" }}>{items.toString()}</h1>
   );
+  useEffect(() => {
+    var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+    const options = {
+        backgroundColor: "transparent",
+        data: [
+          {
+            type: "pie",
+            click: function (e: {
+              dataSeries: { dataPoints: string | any[] };
+              dataPoint: { exploded: boolean };
+            }) {
+              for (var i = 0; i < e.dataSeries.dataPoints.length; i++) {
+                e.dataSeries.dataPoints[i].exploded = false;
+              }
+              e.dataPoint.exploded = true;
+            },
+            dataPoints: items,
+          },
+        ],
+      },
+      //Styling Chart Container
+      containerProps = {
+        width: "100%",
+        height: "300px",
+      };
+    setPieChart(
+      <CanvasJSChart options={options} containerProps={containerProps} />
+    );
+  }, []);
+
+  return pieChart;
 }
 
 type PageProps = {
