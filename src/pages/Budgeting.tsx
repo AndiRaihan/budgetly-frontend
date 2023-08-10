d8f4dae0891c9e5c1d07d4634cccb9e3121bb799
AndiRaihan <dzaky.raihan@gmail.com>
2023-08-10 19:38:34 +0700

    Finished budgeting dark mode

--- src/pages/Budgeting.tsx
+++ src/pages/Budgeting.tsx
@@ -73,10 +73,14 @@ export default function Budgeting({ translate, changeCurrentPage }: PageProps) {
       <button
         onClick={() => setShowForm((prevState) => !prevState)}
         className={`
-        ${darkMode.isDarkMode ? "text-background-dark-200" : ""}
+        ${
+          darkMode.isDarkMode
+            ? "text-background-dark-200 hover:bg-background-dark-350 hover:bg-opacity-25"
+            : "hover:bg-background-light-200"
+        }
         ${
           !showForm ? "max-h-max p-1 ml-5" : "max-h-0"
-        } transition-all ease-in-out duration-300 shrink-0 self-start text-start hover:bg-background-light-200 rounded-md w-11/12 overflow-hidden`}
+        } transition-all ease-in-out duration-300 shrink-0 self-start text-start rounded-md w-11/12 overflow-hidden`}
       >
         +Add Budget
       </button>
