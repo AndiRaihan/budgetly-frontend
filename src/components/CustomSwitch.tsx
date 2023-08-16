d26d904e3987dcf24a1b1f15ae8118fa0a56fad5
AndiRaihan <dzaky.raihan@gmail.com>
2023-08-16 13:47:36 +0700

    Recolored switch

--- src/components/CustomSwitch.tsx
+++ src/components/CustomSwitch.tsx
@@ -1,15 +1,22 @@
 import { Switch, alpha, styled } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
-const CustomSwitch = styled(Switch)(({ theme }) => ({
-  "& .MuiSwitch-switchBase.Mui-checked": {
-    color: "#9CCCAF",
-    "&:hover": {
-      backgroundColor: alpha("#9CCCAF", theme.palette.action.hoverOpacity),
+const CustomSwitch = styled(Switch)(({ theme }) => {
+  const { darkMode } = useSelector((state: RootState) => state);
+  const color = darkMode.isDarkMode ? "#A5C9CA" : "#9CCCAF";
+
+  return {
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      color: color,
+      "&:hover": {
+        backgroundColor: alpha(color, theme.palette.action.hoverOpacity),
+      },
     },
-  },
-  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-    backgroundColor: "#9CCCAF",
-  },
-}));
+    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+      backgroundColor: color,
+    },
+  };
+});
 
 export default CustomSwitch;
