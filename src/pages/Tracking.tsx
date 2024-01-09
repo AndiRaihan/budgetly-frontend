9b3b5cbb3e82467ec448cabf3c0826dbd31defc9
AndiRaihan <dzaky.raihan@gmail.com>
2024-01-09 21:32:12 +0700

    tracking components added from be

--- src/pages/Tracking.tsx
+++ src/pages/Tracking.tsx
@@ -25,14 +25,14 @@ export default function Home({ translate, changeCurrentPage }: PageProps) {
 
   const [showForm, setShowForm] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [trackings, setTrackings] = useState(trackingData);
+  const [trackings, setTrackings] = useState<any>();
   const [userTrackingData, setUserTrackingData] = useState<any>(null);
   const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     async function fetchTrackingData() {
       const response = await fetch(
-        `https://budgetly-backend-v2-production.up.railway.app/api/v1/tracking/${account.id}?groupBy=time&sortBy=time&type=userId`,
+        `https://budgetly-backend-v2-production.up.railway.app/api/v1/tracking/?groupBy=time&sortBy=time`,
         {
           method: "GET",
           headers: {
@@ -42,81 +42,146 @@ export default function Home({ translate, changeCurrentPage }: PageProps) {
         }
       );
 
-      const trackingResponse: GetAllUserTrackingResponse =
+      let trackingResponse: GetAllUserTrackingResponse =
         Convert.toGetAllUserTrackingResponse(await response.text());
-      setUserTrackingData(trackingResponse);
-      console.log(userTrackingData);
+
+      const modifiedTrackingToday = trackingResponse.today.map((tracking) => {
+        return {
+          id: tracking._id,
+          trackingName: tracking.name,
+          amount: tracking.amount,
+          trackDate: tracking.date,
+          category: tracking.category,
+          trackType: tracking.isExpense,
+          isOpened: false,
+        };
+      });
+
+      const modifiedTrackingYesterday = trackingResponse.yesterday.map(
+        (tracking) => {
+          return {
+            id: tracking._id,
+            trackingName: tracking.name,
+            amount: tracking.amount,
+            trackDate: tracking.date,
+            category: tracking.category,
+            trackType: tracking.isExpense,
+            isOpened: false,
+          };
+        }
+      );
+
+      const modifiedTrackingPast = trackingResponse.past.map((tracking) => {
+        return {
+          id: tracking._id,
+          trackingName: tracking.name,
+          amount: tracking.amount,
+          trackDate: tracking.date,
+          category: tracking.category,
+          trackType: tracking.isExpense,
+          isOpened: false,
+        };
+      });
+
+      const modifiedTrackingData = {
+        today: modifiedTrackingToday,
+        yesterday: modifiedTrackingYesterday,
+        past: modifiedTrackingPast,
+      };
+
+      setTrackings(modifiedTrackingData);
     }
     fetchTrackingData();
   }, [refresh]);
 
-  const { darkMode } = useSelector((state: RootState) => state);
+  useEffect(() => {
+    if (trackings === null || trackings === undefined) return;
+    let trackingComponents = [];
+    const todayData = trackings.today;
+    const yesterdayData = trackings.yesterday;
+    const pastData = trackings.past;
 
-  function showEditForm(id: string) {
-    setTrackings((prev) =>
-      prev.map((tracking) => {
-        if (tracking.id === id) {
-          return { ...tracking, isOpened: true };
-        }
-        return { ...tracking, isOpened: false };
-      })
-    );
-  }
+    if (todayData.length > 0) {
+      const currentDate = new Date();
+      const formattedCurrentDate = currentDate.toLocaleDateString("en-US", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+      });
 
-  function closeForm() {
-    setTrackings((prev) =>
-      prev.map((tracking) => {
-        return { ...tracking, isOpened: false };
-      })
-    );
-  }
-
-  const currentDate = new Date();
-  const formattedDate = currentDate.toLocaleDateString("en-US", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  });
-
-  return (
-    <div
-      className={` h-screen flex flex-col pt-20 items-start px-16
-        ${
-          darkMode.isDarkMode
-            ? "bg-background-dark-400 dark"
-            : "bg-background-light-100"
-        } ${
-        translate
-          ? "translate-x-52 w-[calc(100vw-13rem)]"
-          : " translate-x-0 w-screen"
-      } transition-all ease-in-out duration-200 pb-20`}
-    >
-      <TrackingForm showForm={showForm} setShowForm={setShowForm} />
-      <button
-        onClick={() => setShowForm((prevState) => !prevState)}
-        className={`
-        ${
-          darkMode.isDarkMode
-            ? "text-background-dark-200 hover:bg-background-dark-350 hover:bg-opacity-25"
-            : "hover:bg-background-light-200"
-        }
-        ${
-          !showForm ? "max-h-max p-1 ml-5" : "max-h-0"
-        } transition-all ease-in-out duration-300 flex-shrink-0 self-start text-start rounded-md w-11/12 overflow-hidden`}
-      >
-        +Add Expense/income
-      </button>
-      {showForm && (
-        <hr className="border rounded-md w-11/12 ml-5 border-black" />
-      )}
-      <div className="flex flex-col justify-between p-1 ml-5 mt-3 w-11/12 md:flex-row md:items-end">
+      trackingComponents.push(
         <h1
           className={`text-2xl md:text-3xl ${
             darkMode.isDarkMode ? "text-background-dark-200" : ""
           }`}
         >
-          Today <span className="text-sm">{formattedDate}</span>
+          Today <span className="text-sm">{formattedCurrentDate}</span>
         </h1>
+      );
+
+      todayData.forEach((data: { trackingName: any; amount: any; trackDate: any; category: any; trackType: any; id: string; isOpened: boolean; }) => {
+        trackingComponents.push(
+          <TrackingBar
+            trackingData={{
+              trackingName: data.trackingName,
+              amount: data.amount,
+              trackDate: data.trackDate,
+              category: data.category,
+              trackType: data.trackType,
+            }}
+            closeForm={closeForm}
+            id={data.id}
+            isOpened={data.isOpened}
+            showEditForm={showEditForm}
+          />
+        );
+      });
+    }
+
+    if (yesterdayData.length > 0) {
+      const yesterdayDate = new Date();
+      yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+      const formattedYesterdayDate = yesterdayDate.toLocaleDateString("en-US", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+      });
+
+      trackingComponents.push(
+        <h1
+          className={`text-2xl md:text-3xl ${
+            trackingComponents.length > 0 ? "ml-5 mt-10 " : " "
+          } ${darkMode.isDarkMode ? "text-background-dark-200" : ""}`}
+        >
+          Yesterday <span className="text-sm">{formattedYesterdayDate}</span>
+        </h1>
+      );
+
+      yesterdayData.forEach((data: { trackingName: any; amount: any; trackDate: any; category: any; trackType: any; id: string; isOpened: boolean; }) => {
+        trackingComponents.push(
+          <TrackingBar
+            trackingData={{
+              trackingName: data.trackingName,
+              amount: data.amount,
+              trackDate: data.trackDate,
+              category: data.category,
+              trackType: data.trackType,
+            }}
+            closeForm={closeForm}
+            id={data.id}
+            isOpened={data.isOpened}
+            showEditForm={showEditForm}
+          />
+        );
+      });
+    }
+
+    if (pastData.length > 0) {
+    }
+
+    trackingComponents[0] = (
+      <div className="flex flex-col justify-between p-1 ml-5 mt-3 w-11/12 md:flex-row md:items-end">
+        {trackingComponents[0]}
         <div className={`relative z-50`}>
           <button
             className={` ${
@@ -172,67 +237,103 @@ export default function Home({ translate, changeCurrentPage }: PageProps) {
           </div>
         </div>
       </div>
+    );
 
-      {/* TODO: Kalo bisa refactor aja sih ini section headernya biar jadi component sama tracking bar-nya */}
-      <TrackingBar
-        trackingData={{
-          trackingName: trackings[0].trackingName,
-          amount: trackings[0].amount,
-          trackDate: trackings[0].trackDate,
-          category: trackings[0].category,
-          trackType: trackings[0].trackType,
-        }}
-        closeForm={closeForm}
-        id={trackings[0].id}
-        isOpened={trackings[0].isOpened}
-        showEditForm={showEditForm}
-      />
-      <h1
-        className={`text-3xl ml-5 mt-10 ${
-          darkMode.isDarkMode ? "text-background-dark-200" : ""
-        }`}
+    setUserTrackingData(trackingComponents);
+  }, [trackings]);
+  const { darkMode } = useSelector((state: RootState) => state);
+
+  function showEditForm(id: string) {
+    setTrackings((prev: { today: { map: (arg0: (tracking: any) => any) => never[]; }; yesterday: { map: (arg0: (tracking: any) => any) => never[]; }; past: { map: (arg0: (tracking: any) => any) => never[]; }; }) => {
+      const newTracking = {
+        today: [],
+        yesterday: [],
+        past: [],
+
+      }
+      newTracking.today = prev.today.map((tracking: { id: string; }) => {
+        if (tracking.id === id) {
+          return { ...tracking, isOpened: true };
+        }
+        return { ...tracking, isOpened: false };
+      });
+      newTracking.yesterday = prev.yesterday.map((tracking: { id: string; }) => {
+        if (tracking.id === id) {
+          return { ...tracking, isOpened: true };
+        }
+        return { ...tracking, isOpened: false };
+      });
+      newTracking.past = prev.past.map((tracking: { id: string; }) => {
+        if (tracking.id === id) {
+          return { ...tracking, isOpened: true };
+        }
+        return { ...tracking, isOpened: false };
+      });
+      return newTracking;
+    });
+
+  }
+
+  function closeForm() {
+    setTrackings((prev: { today: { map: (arg0: (tracking: any) => any) => never[]; }; yesterday: { map: (arg0: (tracking: any) => any) => never[]; }; past: { map: (arg0: (tracking: any) => any) => never[]; }; }) => {
+      const newTracking = {
+        today: [],
+        yesterday: [],
+        past: [],
+      };
+      newTracking.today = prev.today.map((tracking: any) => {
+        return { ...tracking, isOpened: false };
+      });
+      newTracking.yesterday = prev.yesterday.map((tracking: any) => {
+        return { ...tracking, isOpened: false };
+      });
+      newTracking.past = prev.past.map((tracking: any) => {
+        return { ...tracking, isOpened: false };
+      });
+      return newTracking;
+    }
+    );
+  }
+
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+
+  return (
+    <div
+      className={` h-screen flex flex-col pt-20 items-start px-16
+        ${
+          darkMode.isDarkMode
+            ? "bg-background-dark-400 dark"
+            : "bg-background-light-100"
+        } ${
+        translate
+          ? "translate-x-52 w-[calc(100vw-13rem)]"
+          : " translate-x-0 w-screen"
+      } transition-all ease-in-out duration-200 pb-20`}
+    >
+      <TrackingForm showForm={showForm} setShowForm={setShowForm} />
+      <button
+        onClick={() => setShowForm((prevState) => !prevState)}
+        className={`
+        ${
+          darkMode.isDarkMode
+            ? "text-background-dark-200 hover:bg-background-dark-350 hover:bg-opacity-25"
+            : "hover:bg-background-light-200"
+        }
+        ${
+          !showForm ? "max-h-max p-1 ml-5" : "max-h-0"
+        } transition-all ease-in-out duration-300 flex-shrink-0 self-start text-start rounded-md w-11/12 overflow-hidden`}
       >
-        Yesterday
-      </h1>
-      <TrackingBar
-        trackingData={{
-          trackingName: trackings[1].trackingName,
-          amount: trackings[1].amount,
-          trackDate: trackings[1].trackDate,
-          category: trackings[1].category,
-          trackType: trackings[1].trackType,
-        }}
-        closeForm={closeForm}
-        id={trackings[1].id}
-        isOpened={trackings[1].isOpened}
-        showEditForm={showEditForm}
-      />
-      <TrackingBar
-        trackingData={{
-          trackingName: trackings[2].trackingName,
-          amount: trackings[2].amount,
-          trackDate: trackings[2].trackDate,
-          category: trackings[2].category,
-          trackType: trackings[2].trackType,
-        }}
-        closeForm={closeForm}
-        id={trackings[2].id}
-        isOpened={trackings[2].isOpened}
-        showEditForm={showEditForm}
-      />
-      <TrackingBar
-        trackingData={{
-          trackingName: trackings[3].trackingName,
-          amount: trackings[3].amount,
-          trackDate: trackings[3].trackDate,
-          category: trackings[3].category,
-          trackType: trackings[3].trackType,
-        }}
-        closeForm={closeForm}
-        id={trackings[3].id}
-        isOpened={trackings[3].isOpened}
-        showEditForm={showEditForm}
-      />
+        +Add Expense/income
+      </button>
+      {showForm && (
+        <hr className="border rounded-md w-11/12 ml-5 border-black" />
+      )}
+      {userTrackingData}
     </div>
   );
 }
